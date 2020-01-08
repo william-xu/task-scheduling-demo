82e7a4e9e5f0b732b3419f2508deb51b1c99092b
william-xu <1102793+william-xu@users.noreply.github.com>
2020-01-08 18:41:26 +0800

    init

---
+++ src/test/java/org/xwl/task/TaskSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xwl.task;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TaskSchedulingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
